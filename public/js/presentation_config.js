42de1527f91cc470fff0029d31fc2f57373833db
Maria Dubovitskaya <mdu@zurich.ibm.com>
2015-10-14 03:18:41 -0700

    configured the example mplex

--- public/js/presentation_config.js
+++ public/js/presentation_config.js
@@ -1,4 +1,16 @@
 // This object should contain the results
 // of Verifier service setup
 
-var verifier_config = {};
+var verifier_config = {
+  "issuer_data": null,
+  "verifier_data": [
+    {
+      "uid": "idmx:bluemix://idmx-directory.mybluemix.net/policies?type=presentation,name=idemix_example_valid_voucher,version=1",
+      "friendlyName": "Idemix Example Valid Voucher"
+    },
+    {
+      "uid": "idmx:bluemix://idmx-directory.mybluemix.net/policies?type=presentation,name=mplex_example_over_16_from_e_gov,version=1",
+      "friendlyName": "Mplex Example Over 16 from eGov"
+    }
+  ]
+};
