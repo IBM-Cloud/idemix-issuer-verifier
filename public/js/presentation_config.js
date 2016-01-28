1fedd4d179acfdefad4beb369aa2d93bf52fe555
idemix <idemix@zurich.ibm.com>
2016-01-28 14:44:56 +0100

    switched to another policy

--- public/js/presentation_config.js
+++ public/js/presentation_config.js
@@ -18,8 +18,8 @@ var verifier_config = {
   "issuer_data": null,
   "verifier_data": [
     {
-      "uid": "idmx:bluemix://idmx-directory.mybluemix.net/policies?type=presentation,name=idemix_example_valid_voucher,version=1",
-      "friendlyName": "Idemix Example Valid Voucher"
+      "uid": "idmx:bluemix://idmx-directory.mybluemix.net/policies?type=presentation,name=mplex_example_valid_voucher,version=1",
+      "friendlyName": "Mplex Example Valid Voucher"
     },
     {
       "uid": "idmx:bluemix://idmx-directory.mybluemix.net/policies?type=presentation,name=mplex_example_over_16_from_e_gov,version=1",
